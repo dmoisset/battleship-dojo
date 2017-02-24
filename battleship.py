920eef41470f4d5a4984150578f4b83793fc0b81
Daniel F Moisset <dmoisset@machinalis.com>
2017-02-24 21:15:18 +0000

    Added comments

--- battleship.py
+++ battleship.py
@@ -1,3 +1,5 @@
+# A board is a list of rows, and each row is a list of cells with either an 'X' (a battleship)
+# or a blank ' ' (water)
 board = [
     [' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' '],
@@ -6,6 +8,9 @@ board = [
     [' ', ' ', ' ', ' ', ' '],
 ]
 
+# We want to refer to columns by letter, but Python accesses lists by number. So we define
+# a dictionary to translate letters to the corresponding number. Note that Python lists start in
+# zero, not in one!
 letters_to_numbers = {
     'A': 0,
     'B': 1,
@@ -14,14 +19,20 @@ letters_to_numbers = {
     'E': 4,
 }
 
+# We want 5 battleships, so we use a for loop to ask for a ship 5 times!
 for n in range(5):
     print("Where do you want ship ", n + 1, "?")
     column = input("column (A to E):")
     row = input("row (1 to 5):")
+    # columns are letters, so here we use the dictionary to get the number corresponding to the
+    # letter
     column_number = letters_to_numbers[column]
+    # The player enters numbers from 1 to 5, but we have to substract 1 to use python lists that
+    # start on zero.
     row_number = int(row) - 1
 
     board[row_number][column_number] = 'X'
 
+    # Show the board, one row at a time
     for row in board:
         print(row)
